d685d1913837065cbfac18c5d434091ab6d48a04
Tim Williams <tikytimbo5@yahoo.com>
2017-07-12 13:44:57 -0600

    added doctype - hooray! :joy_cat:

--- index.php
+++ index.php
@@ -6,5 +6,17 @@
 	</head>
 	<body>
 		<h1>This is my web page</h1>
+		<table>
+			<th>
+			<td>Cats</td>
+			<td>Dogs</td>
+			</th>
+			<tr>
+				<td>Persian</td>
+				<td>Beagle</td>
+			</tr>
+			<tr></tr>
+			<tr></tr>
+		</table>
 	</body>
 </html>
