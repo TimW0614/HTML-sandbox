2d62be5e865e102c168bccda60b882bbb16cecae
Tim Williams <tikytimbo5@yahoo.com>
2017-07-31 11:46:06 -0600

    cleaned project for Bootstrap exercise

--- index.php
+++ index.php
@@ -16,6 +16,7 @@
 		<link type="text/css" href="//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
 
 		<!-- CUSTOM CSS HERE -->
+		<link rel="stylesheet" href="css/style.css">
 
 		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 		<!--[if lt IE 9]>
@@ -31,42 +32,106 @@
 
 		<title>Bootstrap Practice</title>
 	</head>
-	<body>
-		<!-- begin grid layout -->
-		<div class="container">
-			<div class="row">
-				<div class="col-md-6">
-					<h2>Column 1</h2>
-					<p>Deflector power at maximum. Energy discharge in six seconds. Warp reactor core primary coolant failure. Fluctuate phaser resonance frequencies. Resistance is futile. Recommend we adjust shield harmonics to the upper EM band when proceeding. These appear to be some kind of power-wave-guide conduits which allow them to work collectively as they perform ship functions. Increase deflector modulation to upper frequency band.</p>
-				</div>
-				<div class="col-md-6">
-					<h2>Column 2</h2>
-					<p>We're acquainted with the wormhole phenomenon, but this... Is a remarkable piece of bio-electronic engineering by which I see much of the EM spectrum ranging from heat and infrared through radio waves, et cetera, and forgive me if I've said and listened to this a thousand times. This planet's interior heat provides an abundance of geothermal energy. We need to neutralize the homing signal.</p>
-				</div><!-- /.col-md-6 -->
-			</div><!-- /.row -->
+	<body class="sfooter">
+		<div class="sfooter-content">
+			<header>
+				<nav class="navbar navbar-default">
+					<div class="container">
+						<!-- Brand and toggle get grouped for better mobile display -->
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main-nav" aria-expanded="false">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+							<a class="navbar-brand" href="#">Bootstrap Sandbox</a>
+						</div>
 
-			<div class="row">
-				<div class="col-md-12 hidden-xs">
-					<h2>Column 3</h2>
-					<p>Shields up. I recommend we transfer power to phasers and arm the photon torpedoes. Something strange on the detector circuit. The weapons must have disrupted our communicators. You saw something as tasty as meat, but inorganically materialized out of patterns used by our transporters. Captain, the most elementary and valuable statement in science, the beginning of wisdom, is 'I do not know.' All transporters off. Shields up. I recommend we transfer power to phasers and arm the photon torpedoes. Something strange on the detector circuit. The weapons must have disrupted our communicators. You saw something as tasty as meat, but inorganically materialized out of patterns used by our transporters. Captain, the most elementary and valuable statement in science, the beginning of wisdom, is 'I do not know.' All transporters off.</p>
+						<!-- Collect the nav links, forms, and other content for toggling -->
+						<div class="collapse navbar-collapse" id="main-nav">
+							<ul class="nav navbar-nav navbar-right">
+								<li><a href="https://www.abebooks.com/" target="_blank">AbeBooks</a></li>
+								<li><a href="https://www.groupon.com/" target="_blank">Groupon</a></li>
+								<li><a href="https://www.jetpens.com/" target="_blank">JetPens</a></li>
+								<li><a href="https://customcultsnowboards.com/index.php" target="_blank">CustomCult</a></li>
+							</ul>
+						</div><!-- /.navbar-collapse -->
+					</div><!-- /.container-fluid -->
+				</nav>
+			</header>
+
+			<!-- begin grid layout -->
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6">
+						<h2>Column 1</h2>
+						<p>Deflector power at maximum. Energy discharge in six seconds. Warp reactor core primary coolant failure. Fluctuate phaser resonance frequencies. Resistance is futile. Recommend we adjust shield harmonics to the upper EM band when proceeding. These appear to be some kind of power-wave-guide conduits which allow them to work collectively as they perform ship functions. Increase deflector modulation to upper frequency band.</p>
+					</div>
+					<div class="col-md-6">
+						<h2>Column 2</h2>
+						<p>We're acquainted with the wormhole phenomenon, but this... Is a remarkable piece of bio-electronic engineering by which I see much of the EM spectrum ranging from heat and infrared through radio waves, et cetera, and forgive me if I've said and listened to this a thousand times. This planet's interior heat provides an abundance of geothermal energy. We need to neutralize the homing signal.</p>
+					</div><!-- /.col-md-6 -->
+				</div><!-- /.row -->
+
+				<div class="row">
+					<div class="col-md-12 hidden-xs">
+						<h2>Column 3</h2>
+						<p>Shields up. I recommend we transfer power to phasers and arm the photon torpedoes. Something strange on the detector circuit. The weapons must have disrupted our communicators. You saw something as tasty as meat, but inorganically materialized out of patterns used by our transporters. Captain, the most elementary and valuable statement in science, the beginning of wisdom, is 'I do not know.' All transporters off. Shields up. I recommend we transfer power to phasers and arm the photon torpedoes. Something strange on the detector circuit. The weapons must have disrupted our communicators. You saw something as tasty as meat, but inorganically materialized out of patterns used by our transporters. Captain, the most elementary and valuable statement in science, the beginning of wisdom, is 'I do not know.' All transporters off.</p>
+					</div><!--/.row-->
+				</div><!-- /.row -->
+
+				<div class="row">
+					<div class="col-md-4">
+						<h2>Column 4</h2>
+						<p>Shields up. I recommend we transfer power to phasers and arm the photon torpedoes. Something strange on the detector circuit. The weapons must have disrupted our communicators. You saw something as tasty as meat, but inorganically materialized out of patterns used by our transporters. Captain, the most elementary and valuable statement in science, the beginning of wisdom, is 'I do not know.' All transporters off.</p>
+					</div>
+					<div class="col-md-4">
+						<h2>Column 5</h2>
+						<p>Shields up. I recommend we transfer power to phasers and arm the photon torpedoes. Something strange on the detector circuit. The weapons must have disrupted our communicators. You saw something as tasty as meat, but inorganically materialized out of patterns used by our transporters. Captain, the most elementary and valuable statement in science, the beginning of wisdom, is 'I do not know.' All transporters off.</p>
+					</div>
+					<div class="col-md-4">
+						<h2>Column 6</h2>
+						<p>Shields up. I recommend we transfer power to phasers and arm the photon torpedoes. Something strange on the detector circuit. The weapons must have disrupted our communicators. You saw something as tasty as meat, but inorganically materialized out of patterns used by our transporters. Captain, the most elementary and valuable statement in science, the beginning of wisdom, is 'I do not know.' All transporters off.</p>
+					</div>
 				</div><!--/.row-->
-			</div><!-- /.row -->
 
-			<div class="row">
-				<div class="col-md-4">
-					<h2>Column 4</h2>
-					<p>Shields up. I recommend we transfer power to phasers and arm the photon torpedoes. Something strange on the detector circuit. The weapons must have disrupted our communicators. You saw something as tasty as meat, but inorganically materialized out of patterns used by our transporters. Captain, the most elementary and valuable statement in science, the beginning of wisdom, is 'I do not know.' All transporters off.</p>
+				<div class="row">
+					<div class="col-md-4">
+						<h2>Contact Me!</h2>
+						<!-- BEGIN FORM -->
+						<form>
+							<div class="form-group">
+								<label for="txtName">Your Name</label>
+								<input id="txtName" name="txtName" class="form-control" type="text" placeholder="Name">
+							</div>
+							<div class="form-group">
+								<label for="emailEmail">Email Address</label>
+								<input id="emailEmail" name="emailEmail" class="form-control" type="email" placeholder="Email Address">
+							</div>
+							<div class="form-group">
+								<label for="txtSubject">Subject</label>
+								<input id="txtSubject" name="txtSubject" class="form-control" type="text" placeholder="Subject">
+							</div>
+							<div class="form-group">
+								<label for="txtMessage">Message</label>
+								<textarea id="txtMessage" name="txtMessage" class="form-control" cols="30" rows="10"></textarea>
+							</div>
+							<button class="btn btn-info" type="submit">Submit</button>
+							<button class="btn btn-default" type="reset">Reset</button>
+						</form>
+					</div>
+					<div class="col-md-7 col-md-offset-1">
+						<h2>More Info</h2>
+						<p>Sensors indicate human life forms 30 meters below the planet's surface. Stellar flares are increasing in magnitude and frequency. Set course for Rhomboid Dronegar 006, warp seven. There's no evidence of an advanced communication network. Total guidance system failure, with less than 24 hours' reserve power. Shield effectiveness has been reduced 12 percent. We have covered the area in a spherical pattern which a ship without warp drive could cross in the given time.</p>
+						<p>Sensors indicate no shuttle or other ships in this sector. According to coordinates, we have travelled 7,000 light years and are located near the system J-25. Tractor beam released, sir. Force field maintaining our hull integrity. Damage report? Sections 27, 28 and 29 on decks four, five and six destroyed. Without our shields, at this range it is probable a photon detonation could destroy the Enterprise.</p>
+						<p>Unidentified vessel travelling at sub warp speed, bearing 235.7. Fluctuations in energy readings from it, Captain. All transporters off. A strange set-up, but I'd say the graviton generator is depolarized. The dark colourings of the scrapes are the leavings of natural rubber, a type of non-conductive sole used by researchers experimenting with electricity. The molecules must have been partly de-phased by the anyon beam.</p>
+						<p>Shields up. I recommend we transfer power to phasers and arm the photon torpedoes. Something strange on the detector circuit. The weapons must have disrupted our communicators. You saw something as tasty as meat, but inorganically materialized out of patterns used by our transporters. Captain, the most elementary and valuable statement in science, the beginning of wisdom, is 'I do not know.' All transporters off.</p>
+					</div>
 				</div>
-				<div class="col-md-4">
-					<h2>Column 5</h2>
-					<p>Shields up. I recommend we transfer power to phasers and arm the photon torpedoes. Something strange on the detector circuit. The weapons must have disrupted our communicators. You saw something as tasty as meat, but inorganically materialized out of patterns used by our transporters. Captain, the most elementary and valuable statement in science, the beginning of wisdom, is 'I do not know.' All transporters off.</p>
-				</div>
-				<div class="col-md-4">
-					<h2>Column 6</h2>
-					<p>Shields up. I recommend we transfer power to phasers and arm the photon torpedoes. Something strange on the detector circuit. The weapons must have disrupted our communicators. You saw something as tasty as meat, but inorganically materialized out of patterns used by our transporters. Captain, the most elementary and valuable statement in science, the beginning of wisdom, is 'I do not know.' All transporters off.</p>
-				</div>
-			</div><!--/.row-->
-		</div><!-- /.container -->
+
+			</div><!-- /.container -->
+		</div><!--/.sfooter-content-->
 
 		<footer>
 			<div class="container">
@@ -79,6 +144,3 @@
 		</footer>
 	</body>
 </html>
-
-
-
